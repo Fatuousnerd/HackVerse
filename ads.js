5f0d5c38968c4d927370321948f9a532bd218178
fatuousnerd <110926333+Fatuousnerd@users.noreply.github.com>
2023-07-01 20:35:01 +0300

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'e3dbeb7a538a0ea7ff5378cd815df67a',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/e3dbeb7a538a0ea7ff5378cd815df67a/invoke.js"></scr' + 'ipt>');
